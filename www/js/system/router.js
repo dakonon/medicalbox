3711145cf870cfd453fec3e90328f707d23d9986
Pablo Gonzalez <pdonaire1@gmail.com>
2016-07-04 13:23:34 -0430

    Animations effects in dashboard with transition

--- www/js/system/router.js
+++ www/js/system/router.js
@@ -40,14 +40,14 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
+  .state('dash', {
     url: '/dash',
-    views: {
-      'tab-dash': {
+    // views: {
+      // 'tab-dash': {
         templateUrl: 'templates/tab-dash.html',
         controller: 'DashCtrl'
-      }
-    }
+      // }
+    // }
   })
 
   .state('tab.chats', {
@@ -80,6 +80,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/dash');
 
 });
