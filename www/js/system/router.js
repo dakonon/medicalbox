7d2fae4f54e4703748e8dce35bd2c66911bf9b4b
jose752 <jagr_753@hotmail.com>
2016-07-05 17:13:19 -0400

    nuevos cambios

--- www/js/system/router.js
+++ www/js/system/router.js
@@ -78,7 +78,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     );
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/dash');
 
 
 
