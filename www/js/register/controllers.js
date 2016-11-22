883cc01ad8dbf9335183f3d2bd018b274f7d9f87
rudmanmrrod <rudman22@hotmail.com>
2016-11-22 15:40:08 -0400

    Added specific field to patient/doctor

--- www/js/register/controllers.js
+++ www/js/register/controllers.js
@@ -6,11 +6,8 @@
 		$scope.data = {};
 
 		$scope.send = function(){
-			console.log($scope.data.user_type);
-			var parameter = {"email":$scope.data.email,"password":$scope.data.password,"first_name":$scope.data.first_name,
-			"last_name":$scope.data.last_name,"phone_number":$scope.data.phone_number,"address":$scope.data.address};
 			RegisterService.onRegister($scope.data.email,$scope.data.password,$scope.data.first_name,
-			$scope.data.last_name,$scope.data.phone_number,$scope.data.address,$scope.data.user_type)
+			$scope.data.last_name,$scope.data.phone_number,$scope.data.address,$scope.data.user_type,$scope.data.profession)
 			.success(function(data) {
 			console.log(data);
 			$ionicPopup.alert({
--- www/js/register/services.js
+++ www/js/register/services.js
@@ -10,14 +10,14 @@ function RegisterService($http,$q,constants) {
 
     this.onRegister = onRegister;
   
-    function onRegister(email, password, first_name, last_name, phone_number, address, user_type){
+    function onRegister(email, password, first_name, last_name, phone_number, address, user_type, profession){
       var deferred = $q.defer();
       var promise = deferred.promise;
       
-      var params = {"email":email,"password":password,"first_name":first_name,
-		"last_name":last_name,"phone_number":phone_number,"address":address};
       if(user_type=='Patient')
       {
+      	     var params = {"email":email,"password":password,"first_name":first_name,
+		"last_name":last_name,"phone_number":phone_number,"address":address};
       	      var url = constants.register.patients();
 	      $http.post(url, params).then(function(response){
 		if (response.data)
@@ -31,6 +31,8 @@ function RegisterService($http,$q,constants) {
      }
      else if (user_type=="Doctor")
      {
+              var params = {"email":email,"password":password,"first_name":first_name,
+		"last_name":last_name,"phone_number":phone_number,"name":profession};
       	      var url = constants.register.doctors();
 	      $http.post(url, params).then(function(response){
 		if (response.data)
